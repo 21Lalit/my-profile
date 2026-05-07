64e62cd53d1599f1df81ad7ba1913369bb068cd5
21Lalit <167332240+21Lalit@users.noreply.github.com>
2026-05-07 17:24:33 +0000

    Deploying to gh-pages from @ 21Lalit/my-profile@3d40b3599fc7c52ca87229c06e5abb72515dcd8f 🚀

--- _next/static/FWafvDyCqSSCPEwxGofyO/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/b637e9a5-6cf11adc33c7fdd4.js","static/chunks/14-5293d81a84396ed7.js","static/chunks/pages/index-3c68d193dc12cea7.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
